67dabb5bcd8b293c7ccaab220b62e1c286e7fa7a
Nhat Nguyen Khac <nhatnk1@fsoft.com.vn>
2015-05-06 14:52:16 +0700

    third commit

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 /**
  * Created by macmini on 5/4/15.
  */
+var express     = require('express');
+var app         = express();
+var config      = require('./config');
+var bodyParser  = require('body-parser');
+var mongoose    = require('mongoose');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(require('./app/controllers'));
+app.set('secret', config.secret);
+
+mongoose.connect(config.database);
+
+var port = process.env.PORT || 8080;
+
+app.listen(port);
